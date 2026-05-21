1d71bfef2746d770223d8d846f990a841e6011ad
Serheiev Maksym <01022019serf@gmail.com>
2026-05-21 23:55:50 +0300

    Add user login endpoint with JWT token generation and password verification

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -4,6 +4,9 @@ from database import get_db
 import models
 from schemas.users import UserCreate, UserResponse
 from services.security import get_password_hash
+from fastapi.security import OAuth2PasswordRequestForm
+from schemas.users import Token
+from services.security import verify_password, create_access_token
 
 router = APIRouter(
     prefix="/auth",
@@ -25,10 +28,22 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
         password_hash=hashed_password,
         name=user.name
     )
-
-    # Зберігаємо в базу даних
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-
     return new_user
+
+
+@router.post("/login", response_model=Token)
+def login_user(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == form_data.username).first()
+
+    if not user or not verify_password(form_data.password, user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Невірний email або пароль",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(data={"sub": str(user.id)})
+
+    return {"access_token": access_token, "token_type": "bearer"}
