586b0c37a627a0863710c47492f3945ac8663905
Serheiev Maksym <01022019serf@gmail.com>
2026-05-26 20:43:31 +0300

    Add CORS middleware to FastAPI application

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from routers import auth, rooms, maps, expenses
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(
-    title="RouteSplitter API",
-    description="Бекенд MVP",
-    version="0.1.0"
+app = FastAPI(title="RouteSplitter API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth.router)
